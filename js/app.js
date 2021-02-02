4cc9f6621e2b23fd0ad7fa8f32887fcb87c6bab5
raha moosavi <rahamoosavi1378@gmail.com>
2021-02-02 15:54:52 +0100

    add animation and fix bug

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-const boxText = document.querySelectorAll(".boxText");
-const boxMore = document.querySelectorAll(".boxMore");
+let boxText = document.querySelectorAll(".boxText");
+let boxMore = document.querySelectorAll(".boxMore");
 const lang = document.querySelector("#lang");
 const align = document.querySelector("#align");
 const title = document.querySelector("#title");
@@ -49,6 +49,52 @@ let toggleAlineText = () => {
     console.log(text, view);
 };
 
-let toggleBoxMore = (id = null) => {
-    boxText[--id].querySelector(".boxMore").classList.toggle("hidden");
-};
+function toggleBoxMore(id = null) {
+    let boxMore_ = boxMore[--id];
+    console.log(boxMore_);
+    // boxMore_.classList.toggle("hidden");
+    setTimeout(() => {
+        boxMore_.classList.toggle("boxMoretoggle");
+    }, 200);
+}
+
+let eraser = document.querySelector(".mdi-eraser");
+eraser.addEventListener("click", () => {
+    eraser.classList.add("animate__animated", "animate__bounceIn");
+    setTimeout(() => {
+        eraser.classList.remove("animate__animated", "animate__bounceIn");
+    }, 1000);
+    eraser.style.color = "ff2d2d";
+    title.value = "";
+    comment.value = "";
+});
+
+let heart = document.querySelector(".mdi-heart");
+heart.addEventListener("click", () => {
+    heart.classList.add("animate__animated", "animate__bounceIn");
+    heart.classList.toggle("mdi-heart-ative");
+    setTimeout(() => {
+        heart.classList.remove("animate__animated", "animate__bounceIn");
+        console.log("lol");
+    }, 1000);
+});
+
+let eye = document.querySelector(".mdi-eye");
+eye.addEventListener("click", () => {
+    eye.classList.add("animate__animated", "animate__bounceIn");
+    eye.classList.toggle("mdi-eye-active");
+    setTimeout(() => {
+        eye.classList.remove("animate__animated", "animate__bounceIn");
+        console.log("lol");
+    }, 1000);
+});
+
+let folder = document.querySelector(".mdi-folder");
+folder.addEventListener("click", () => {
+    folder.classList.add("animate__animated", "animate__bounceIn");
+    folder.classList.toggle("mdi-folder-active");
+    setTimeout(() => {
+        folder.classList.remove("animate__animated", "animate__bounceIn");
+        console.log("lol");
+    }, 1000);
+});
